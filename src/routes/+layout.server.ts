92225852d8073b0306574a16e914f092e3b0bf50
mori <chohyuk0725@gmail.com>
2023-05-29 23:17:14 +0900

    환경변수 변경

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,15 +1,9 @@
-export const load = ({ locals }) => {
-	const user = locals.session.user;
-	return {
-		user
-	};
-};
-
 import { VERCEL_COMMIT_REF } from '$env/static/private';
 
 /** @type {import('./$types').LayoutServerLoad} */
 export function load() {
 	return {
-		deploymentGitBranch: VERCEL_COMMIT_REF
+		deploymentGitBranch: VERCEL_COMMIT_REF,
+		user
 	};
 }
