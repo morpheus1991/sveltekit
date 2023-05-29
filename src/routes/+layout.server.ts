864463a0022919c4f8e4510e05d2ca516934b0cb
mori <chohyuk0725@gmail.com>
2023-05-29 23:15:11 +0900

    환경변수 변경

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,3 +4,12 @@ export const load = ({ locals }) => {
 		user
 	};
 };
+
+import { VERCEL_COMMIT_REF } from '$env/static/private';
+
+/** @type {import('./$types').LayoutServerLoad} */
+export function load() {
+	return {
+		deploymentGitBranch: VERCEL_COMMIT_REF
+	};
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,7 @@
 
 	/** @type {import('./$types').LayoutServerData} */
 	export let data;
+	console.log(data);
 
 	console.log('process.env.BASE_URL', process.env.BASE_URL);
 	console.log('process.env.VERCEL_URL', process.env.VERCEL_URL);
