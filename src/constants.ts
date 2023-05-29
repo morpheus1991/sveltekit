90d420ad55a20bcdd4c34bcca2586e00f0047f70
mori <chohyuk0725@gmail.com>
2023-05-29 23:28:12 +0900

    환경변수 변경

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-import { VERCEL_URL, BASE_URL, VITE_VERCEL_URL } from '$env/static/private';
+import { VITE_VERCEL_URL } from '$env/static/private';
 
-export const baseUrl = BASE_URL || VERCEL_URL || VITE_VERCEL_URL;
+export const baseUrl = VITE_VERCEL_URL;
