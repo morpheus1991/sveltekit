36df2816a8f4b6038cd47678b8bcd912fcefd1bf
mori <chohyuk0725@gmail.com>
2023-06-06 21:32:28 +0900

    글쓰기

--- src/routes/boardList/add/+page.svelte
+++ src/routes/boardList/add/+page.svelte
@@ -7,30 +7,43 @@
 	let title: string;
 	let text: string;
 	let isWrittedAdmin: boolean = false;
+
+	/** @type {import('./$types').LayoutServerData} */
+	export let data;
+
+	console.log('data', data);
+	const baseUrl = `https://${data.deploymentGitBranch.VERCEL_URL}`;
 </script>
 
 <div>
-	<form action="">
-		<fieldset>
-			<legend>add</legend>
-			<label for="title">title</label>
-			<input type="text" name="title" id="title" bind:value={title} />
+	{#if $session}
+		<form action="">
+			<fieldset>
+				<legend>add</legend>
+				<label for="title">title</label>
+				<input type="text" name="title" id="title" bind:value={title} />
 
-			<label for="text" />
-			<input type="text" name="text" id="text" bind:value={text} />
+				<label for="text" />
+				<input type="text" name="text" id="text" bind:value={text} />
 
-			<label for="is_writted_admin" />
-			<input type="text" name="is_writted_admin" id="is_writted_admin" />
-		</fieldset>
+				<label for="is_writted_admin" />
+				<input type="text" name="is_writted_admin" id="is_writted_admin" />
+			</fieldset>
 
-		<button
-			type="submit"
-			on:click={(e) => {
-				e.preventDefault();
-				createBoard({ title, text, is_writted_admin, auth: $session }).then((board) => {
-					goto(`/boardList/${board._id}`);
-				});
-			}}>submit</button
-		>
-	</form>
+			<button
+				type="submit"
+				on:click={(e) => {
+					e.preventDefault();
+					createBoard(baseUrl, {
+						title,
+						text,
+						is_writted_admin: isWrittedAdmin,
+						auth: $session
+					}).then((board) => {
+						goto(`/boardList/${board._id}`);
+					});
+				}}>submit</button
+			>
+		</form>
+	{/if}
 </div>
