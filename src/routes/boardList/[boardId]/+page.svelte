69e44c1414314954d3eb04d03fc9bc477c25042a
mori <chohyuk0725@gmail.com>
2023-06-06 20:19:50 +0900

    환경변수 변경

--- src/routes/boardList/[boardId]/+page.svelte
+++ src/routes/boardList/[boardId]/+page.svelte
@@ -5,7 +5,7 @@
 	import { baseUrl } from '../../../stores';
 
 	/** @type {import('./$types').LayoutServerData} */
-	export let data;
+	// export let data;
 
 	console.log('board List data', data);
 
@@ -19,6 +19,6 @@
 	// });
 </script>
 
-{#if board}
+<!-- {#if board}
 	<h1>게시글{board.title}</h1>
-{/if}
+{/if} -->
