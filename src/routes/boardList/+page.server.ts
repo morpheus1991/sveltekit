c10387df0e9306c2257e1a2b8078ac10d1930868
mori <chohyuk0725@gmail.com>
2023-06-06 20:21:50 +0900

    환경변수 변경

--- src/routes/boardList/+page.server.ts
+++ src/routes/boardList/+page.server.ts
@@ -3,3 +3,11 @@
 // export const load = async ({ locals }: LocalsInfo) => {
 // 	if (!locals.session.user) throw redirect(307, '/login');
 // };
+import { env } from '$env/dynamic/private';
+
+/** @type {import('./$types').LayoutServerLoad} */
+export function load() {
+	return {
+		deploymentGitBranch: env
+	};
+}
--- src/routes/boardList/+page.svelte
+++ src/routes/boardList/+page.svelte
@@ -2,16 +2,21 @@
 	import { onMount } from 'svelte';
 	import { getBoards } from '../../service/board';
 
-	let boards = [];
-	onMount(async () => {
-		const data = await getBoards();
-		console.log(data);
-		boards = data;
-	});
+	/** @type {import('./$types').LayoutServerData} */
+	export let data;
+
+	console.log('board List data', data);
+	// let boards = [];
+	// onMount(async () => {
+	// 	const data = await getBoards();
+	// 	console.log(data);
+	// 	boards = data;
+	// });
 </script>
 
+<!-- 
 {#each boards as board}
 	<div>{board._id}</div>
-{/each}
+{/each} -->
 
 <a href="./boardList/add">글쓰기</a>
