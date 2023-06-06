99f05f9823e8edef01cfb88654e333814eb23efd
mori <chohyuk0725@gmail.com>
2023-06-06 19:53:40 +0900

    환경변수 변경

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,1 @@
-/** @type {import('./$types').LayoutServerData} */
-export let data;
-
-console.log(data);
-export const testUrl = data;
-export const baseUrl = import.meta.env.VITE_VERCEL_URL;
+export const testUrl = import.meta.env.VITE_VERCEL_URL;
